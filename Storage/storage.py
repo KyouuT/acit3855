8258f156ad314965f942d5cbd9f3f7c3772759ca
Juan Tarigan <tariganj284@gmail.com>
2025-03-17 19:22:03 -0700

    edit

--- Storage/storage.py
+++ Storage/storage.py
@@ -4,7 +4,7 @@ import yaml
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-APP_CONFIG_PATH = os.path.join(BASE_DIR, "config", "storage_conf.yml")
+APP_CONFIG_PATH = os.path.join(BASE_DIR, "config", "prod", "storage_conf.yml")
 
 with open(APP_CONFIG_PATH, "r") as f:
     app_config = yaml.safe_load(f.read())
